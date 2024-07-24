3e908585b1a0a725a6303177b55960f9744a3731
han-mg <alsrb1277@gmail.com>
2024-07-25 03:48:30 +0900

    [fix] exception in LionService, LionController, ProjectService, ProjectController

---
+++ src/main/java/likelion/sku_sku/exception/IllegalTitleException.java
@@ -0,0 +1,7 @@
+package likelion.sku_sku.exception;
+
+public class IllegalTitleException extends RuntimeException {
+    public IllegalTitleException(String message) {
+        super(message);
+    }
+}

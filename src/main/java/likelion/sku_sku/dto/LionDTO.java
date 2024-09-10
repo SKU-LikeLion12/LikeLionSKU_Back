0b132e04b3440b835adb4463a4a36c3f5bed64cc
unknown <aitchhhan@gmail.com>
2024-09-10 10:50:37 +0900

    [fix] refactoring all DTO

--- src/main/java/likelion/sku_sku/dto/LionDTO.java
+++ src/main/java/likelion/sku_sku/dto/LionDTO.java
@@ -7,46 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 public class LionDTO {
-// Request
-    @Data
-    public static class LionCreateRequest {
-        @Schema(description = "이름", example = "한민규")
-        private String name;
-
-        @Schema(description = "이메일", example = "alswb0830@sungkyul.ac.kr")
-        private String email;
-
-        @Schema(description = "트랙", example = "BACKEND or FRONTEND or PM_DESIGN")
-        private TrackType trackType;
-
-        @Schema(description = "권한", example = "ADMIN_LION or BABY_LION")
-        private RoleType roleType;
-    }
-
-    @Data
-    public static class LionUpdateRequest {
-        @Schema(description = "lion 아이디", example = "1")
-        private Long id;
-
-        @Schema(description = "이름", example = "한민규")
-        private String name;
-
-        @Schema(description = "이메일", example = "alswb0830@sungkyul.ac.kr")
-        private String email;
-
-        @Schema(description = "트랙", example = "BACKEND or FRONTEND or PM_DESIGN")
-        private TrackType trackType;
-
-        @Schema(description = "권한", example = "ADMIN_LION or BABY_LION")
-        private RoleType roleType;
-    }
-
-    @Data
-    public static class LionNameRequest {
-        @Schema(description = "이름", example = "한민규")
-        private String name;
-    }
-
 // Response
     @Data
     @AllArgsConstructor
@@ -62,12 +22,5 @@ public class LionDTO {
 
         @Schema(description = "권한", example = "ADMIN_LION or BABY_LION")
         private RoleType roleType;
-    }
-
-    @Data
-    @AllArgsConstructor
-    public static class NameRole {
-        private String name;
-        private RoleType role;
     }
 }
